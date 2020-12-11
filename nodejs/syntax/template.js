2cb2e1bf2cf8656720f9352b8583e8dd3d8cae67
Caumannerman <uigohiiming2@gmail.com>
2020-12-11 10:06:49 +0900

    Template literal using ` and ${}

---
+++ nodejs/syntax/template.js
@@ -0,0 +1,10 @@
+var name = 'js';
+var letter = `name  ${name}
+
+
+dsjlkfsdjlfjsdklfj ddkfjalsdjf lkdsj 
+sdkjf ldsj fdls dsklfj sdlf jdslk fj${1+1}dslkfjal" + ${name} + name`;
+console.log(letter);
+console.log(letter.length);
+
+// `를 이용한 template literal
